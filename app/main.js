a132f28e1166e5fc4bd49e79198cafc6c40e6a6e
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-09-03 16:57:45 -0600

    Truck and ramp images

--- app/main.js
+++ app/main.js
@@ -6,10 +6,25 @@ let ctx = canvas.getContext("2d")
 // NOTE: The dirtbike in the image is 45.3in tall
 let image = new Image
 image.src = './assets/img/dirtbike.png'
+let truckImg = new Image
+truckImg.src = './assets/img/truckBed.png'
+let rampImg = new Image
+rampImg.src = './assets/img/stepRamp.png'
+
+
+
+function drawImageCenter(image, x, y, cx, cy, scale, rotation){
+		// console.log('Drawing bike')
+		ctx.setTransform(scale, 0, 0, scale, x, y) // sets scale and origin
+		ctx.rotate(rotation)
+		ctx.drawImage(image, -cx, -cy)
+		ctx.setTransform(1,0,0,1,0,0)
+	} 
 
 
 
 let update = function() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	let form = $('form').serializeArray()
 	// console.log(form)
 	let tri = {
@@ -24,30 +39,23 @@ let update = function() {
 	
 	tri.sideC = (Math.sqrt(Math.pow(tri.sideA, 2) - Math.pow(tri.sideB, 2)))
 	tri.angC = Math.asin(tri.sideC / tri.sideA)
-	console.log(tri.angC, tri.angC * (180/Math.PI))
+	// console.log(tri.angC, tri.angC * (180/Math.PI))
 	// console.log(Math.pow(tri.sideA, 2), Math.pow(tri.sideB, 2), (Math.pow(tri.sideA, 2) - Math.pow(tri.sideB, 2)))
 	
+	drawImageCenter(truckImg, -88, 25, 0, 0, 80/truckImg.height*2, 0)
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	ctx.beginPath()
 	ctx.lineJoin = "round"
-	ctx.moveTo(50, 50) // Vertex - ang C
-	ctx.lineTo(50, 50 + tri.sideB * 2) // Vertex - ang A
-	ctx.lineTo(50 + tri.sideC * 2, 50 + tri.sideB * 2) // Vertex - ang B
+	ctx.moveTo(100, 100) // Vertex - ang C
+	ctx.lineTo(100, 100 + tri.sideB * 2) // Vertex - ang A
+	ctx.lineTo(100 + tri.sideC * 2, 100 + tri.sideB * 2) // Vertex - ang B
 	ctx.closePath()
 	ctx.stroke()
 
-	function drawImageCenter(image, x, y, cx, cy, scale, rotation){
-		// console.log('Drawing bike')
-    ctx.setTransform(scale, 0, 0, scale, x, y) // sets scale and origin
-    ctx.rotate(rotation)
-    ctx.drawImage(image, -cx, -cy)
-		ctx.setTransform(1,0,0,1,0,0)
-	} 
-
 	// Drawing the rotated dirtbike image
-	console.log(image.height, 45, 45/image.height)
-	drawImageCenter(image, 50, 50, 0, image.height, 45/image.height*2, (90*Math.PI/180) - tri.angC)
+	// console.log(image.height, 45, 45/image.height)
+	drawImageCenter(rampImg, 100, 100, 0, rampImg.height/2, 72/rampImg.width*2, (90*Math.PI/180) - tri.angC)
+	drawImageCenter(image, 100, 100, 0, image.height, 45/image.height*2, (90*Math.PI/180) - tri.angC)
 	// ctx.drawImage(image, 100, 50)
 	// ctx.setTransform(0.5, 0, 0, 0.5, 50, 50)
 	// ctx.rotate(0.4)
@@ -60,6 +68,7 @@ let update = function() {
 // console.log(ctx)
 if(ctx) {
 	image.addEventListener('load', update)
+	truckImg.addEventListener('load', update)
 	$('form').on('change', update)
 } else {
 	console.error('Canvas did not return a context')
