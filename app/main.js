adbfd0ff62b0f9adca89c77b4f9756d94b6fe20f
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-09-08 15:14:33 -0600

    : Refactor
    Form forces min-max heights
    Can be toggled to draw wireframe in code
    Refactored

--- app/main.js
+++ app/main.js
@@ -1,9 +1,25 @@
-// import { ProxyState } from "./AppState.js";
-
 /**@type {HTMLCanvasElement} */
 let canvas = document.getElementsByTagName('canvas').namedItem('rampCanvas')
 let ctx = canvas.getContext('2d')
 let infoBox = document.getElementById('infoBox')
+let formElem = $('#rampForm')
+let drawWireframe = false
+
+let renderResolution = { width: 2000, height: 1200 }
+let canvasSize = { width: 500, height: 300 }
+
+//  Prevent form submission from redirecting or refreshing
+formElem.on('submit', e => {
+	e.preventDefault()
+})
+
+canvas.width = renderResolution.width
+canvas.height = renderResolution.height
+canvas.style.width = canvasSize.width + 'px'
+canvas.style.height = canvasSize.height + 'px'
+
+let globalScale = renderResolution.width / canvasSize.width
+ctx.setTransform(globalScale, 0, 0, globalScale, 0, 0)
 
 // NOTE: The dirtbike in the image is 45.3in tall
 let bikeImg = new Image()
@@ -20,7 +36,9 @@ let rampB6Img = new Image()
 rampB6Img.src = './assets/img/stepRamp_B6.png'
 
 function drawImageCenter (image, x, y, cx, cy, scale, rotation = 0) {
-	// console.log('Drawing bike')
+	scale = scale * globalScale
+	x = x * globalScale
+	y = y * globalScale
 	ctx.setTransform(scale, 0, 0, scale, x, y) // sets scale and origin
 	ctx.rotate(rotation)
 	ctx.drawImage(image, -cx, -cy)
@@ -29,14 +47,17 @@ function drawImageCenter (image, x, y, cx, cy, scale, rotation = 0) {
 
 let update = function () {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	let form = $('form').serializeArray()
+	let form = $('#rampForm').serializeArray()
 	// console.log(form)
 	let tri = {
 		angA: 90, // Ground Bed
 		angB: undefined, // Ground Ramp
 		angC: undefined, // Bed Ramp
 		sideA: Number(form.find(o => o.name == 'rampModel').value), // Ramp Length
-		sideB: Number(form.find(o => o.name == 'BedHeight').value), // Bed Height
+		sideB: Math.max(
+			30,
+			Math.min(44, Number(form.find(o => o.name == 'BedHeight').value))
+		), // Bed Height
 		sideC: undefined // Ground Depth
 	}
 
@@ -46,57 +67,63 @@ let update = function () {
 	// console.log(tri.angC, tri.angC * (180/Math.PI))
 	// console.log(Math.pow(tri.sideA, 2), Math.pow(tri.sideB, 2), (Math.pow(tri.sideA, 2) - Math.pow(tri.sideB, 2)))
 
-	let rampOrigin = { x: 230, y: 237 - tri.sideB * 2 }
-
-	drawImageCenter(bkgImg, 0, 0, 0, 0, (157 / bkgImg.height) * 2)
-	drawImageCenter(
-		truckImg,
-		0,
-		rampOrigin.y - 76,
-		0,
-		0,
-		(76 / truckImg.height) * 2
-	)
-	drawImageCenter(foreImg, 0, 0, 0, 0, (157 / foreImg.height) * 2)
-
+	let rampOrigin = { x: 230, y: 230 - tri.sideB * 2 }
 	let rampImg = tri.sideA == 72 ? rampB5Img : rampB6Img
 	infoBox.innerHTML = `<div>Based on the input bed height, we recommend the 
-		<span id="recommendedRampModel">${tri.sideB <= 37 ? 'SR-B5' : 'SR-B6'}
+		<span id="recommendedRampModel">${tri.sideB < 36 ? 'SR-B5' : 'SR-B6'}
 		</span> model Step Ramp.</div>
 		<div>Estimated slope angle: ${Math.round(((tri.angC * 180) / Math.PI) * 10) /
 			10}&#176;</div>`
 
-	// Ramp Triangle Wireframe
-	// ctx.beginPath()
-	// ctx.lineJoin = 'round'
-	// ctx.moveTo(rampOrigin.x, rampOrigin.y) // Vertex - ang C
-	// ctx.lineTo(rampOrigin.x, 237) // Vertex - ang A
-	// ctx.lineTo(rampOrigin.x + tri.sideC * 2, 237) // Vertex - ang B
-	// ctx.closePath()
-	// ctx.moveTo(0, 237)
-	// ctx.lineTo(400, 237)
-	// ctx.stroke()
+	if (!drawWireframe) {
+		// Draw the background, then the truck, then the foreground tire
+		console.log(globalScale)
+		console.log('Background scale:', (157 / bkgImg.height) * 2)
+		console.log('Truck scale:', (76 / truckImg.height) * 2)
+		console.log('Truck y-coordinate:', rampOrigin.y - 76)
+		drawImageCenter(bkgImg, 0, 0, 0, 0, (157 / bkgImg.height) * 2)
+		drawImageCenter(
+			truckImg,
+			0,
+			rampOrigin.y - 76,
+			0,
+			0,
+			(76 / truckImg.height) * 2
+		)
+		drawImageCenter(foreImg, 0, 0, 0, 0, (157 / foreImg.height) * 2)
 
-	// Drawing the rotated dirtbike image and ramp image
-	// console.log(image.height, 45, 45/image.height)
-	drawImageCenter(
-		rampImg,
-		rampOrigin.x,
-		rampOrigin.y,
-		0,
-		rampImg.height / 2,
-		(tri.sideA / rampImg.width) * 2,
-		tri.angC
-	)
-	drawImageCenter(
-		bikeImg,
-		rampOrigin.x,
-		rampOrigin.y,
-		0,
-		bikeImg.height,
-		(45 / bikeImg.height) * 2,
-		tri.angC
-	)
+		// Drawing the rotated ramp and dirtbike
+		drawImageCenter(
+			rampImg,
+			rampOrigin.x,
+			rampOrigin.y,
+			0,
+			rampImg.height / 2,
+			(tri.sideA / rampImg.width) * 2,
+			tri.angC
+		)
+		drawImageCenter(
+			bikeImg,
+			rampOrigin.x,
+			rampOrigin.y,
+			0,
+			bikeImg.height,
+			(45 / bikeImg.height) * 2,
+			tri.angC
+		)
+	} else {
+		// Ramp Triangle Wireframe
+		ctx.beginPath()
+		ctx.lineJoin = 'round'
+		ctx.moveTo(rampOrigin.x, rampOrigin.y) // Vertex - ang C
+		ctx.lineTo(rampOrigin.x, 237) // Vertex - ang A
+		ctx.lineTo(rampOrigin.x + tri.sideC * 2, 237) // Vertex - ang B
+		ctx.closePath()
+		// Draw the ground level line
+		ctx.moveTo(0, 237)
+		ctx.lineTo(400, 237)
+		ctx.stroke()
+	}
 }
 
 // console.log(ctx)
